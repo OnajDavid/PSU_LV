c8a3fa02bfa254c7f6846cb3615b8f0d44a74fe3
OnajDavid <101870321+OnajDavid@users.noreply.github.com>
2025-03-05 13:37:14 +0100

    Add files via upload

--- zad5lv1.py
+++ zad5lv1.py
@@ -1,13 +1,17 @@
-file = open('C:\\Users\\student\\Desktop\\LV SU\\song.txt')
-songw ={}
-for line in file:
+song_words = {}
+fsong = open('C:\\Users\\Korisnik\\Downloads\\song.txt')
+for line in fsong:
     line = line.rstrip()
-    words = line.split(" ")
+    words = line.split()
     for word in words:
-        if word in songw.keys():
-            songw[word] += 1
+        if word in song_words:
+            song_words[word] += 1
         else:
-            songw[word] = 1
-print(songw)
-
-file.close()
+            song_words[word] = 1
+counter = 0
+for key,value in song_words.items():
+    if value == 1:
+        counter += 1
+        print(key)
+print(counter)
+fsong.close()
